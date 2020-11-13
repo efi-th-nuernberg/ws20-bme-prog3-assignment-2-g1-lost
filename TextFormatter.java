2b41da801f1800f726c9238be957e19840cd8d81
repl.it user <replituser@example.com>
2020-11-13 14:32:39 +0000

    Assignment 3

--- TextFormatter.java
+++ TextFormatter.java
@@ -1,88 +1,109 @@
+
 import java.util.StringTokenizer;
 
 class TextFormatter {
 
-	private static int maxLineLength;
-	private static String finalText;
-  private static String finalText2;
+  private int maxLineLength;
+  private static String finalText = "";
+  private static String finalText2= "";
+  private static String finalText3 = "";
 
-	private static final String text = "Lorem  ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy "
-			+ "eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et "
-			+ "accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem "
-			+ "ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod "
-			+ "tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et "
-			+ "justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est "
-			+ "Lorem ipsum dolor sit amet.";
+  private static final String text = "Lorem  ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy "
+      + "eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et "
+      + "accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem "
+      + "ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod "
+      + "tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et "
+      + "justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est "
+      + "Lorem ipsum dolor sit amet.";
 
-	public static void main(String[] args) {
-		TextFormatter formatter = new TextFormatter(30);
-		// formatter.print(text);
-		printLeft(text, 30);
-		printRight(text, 30);
-	}
+  public static void main(String[] args) {
+    printLeft(text, 30);
+    printRight(text, 30);
+    printCentral(text, 30);
+  }
 
-	// Konstruktor
-	public TextFormatter(int maxLineLength) {
-		this.maxLineLength = maxLineLength;
-	}
+  // Konstruktor
+  public TextFormatter(int maxLineLength) {
+    this.maxLineLength = maxLineLength;
+  }
 
-	/*
-	 * public static void print (String aText, int maxLineLength) {
-	 * StringTokenizer toki = new StringTokenizer(text); String empty = "";
-	 * while(toki.hasMoreTokens()){ if (empty.length() <= maxLineLength) { empty
-	 * += toki.nextToken() + ' '; //System.out.println(empty); }else{ //empty +=
-	 * "\n"; System.out.println(empty); empty = ""; //System.out.println(empty);
-	 * } } //System.out.println(empty); }
-	 */
-	// Ausgabe
-	public static void printLeft(String aText, int maxLineLength) {
+  // Ausgabe
+  public static void printLeft(String text, int maxLineLength) {
+    StringTokenizer toki = new StringTokenizer(text);
+    String zeile = "";
 
-		StringTokenizer toki = new StringTokenizer(text);
-		String empty = "";
+    while (toki.hasMoreTokens()) {
+      String word = toki.nextToken();
+      if (zeile.length() >= (maxLineLength - word.length())) {
+        finalText = finalText + zeile + "\n";
+        zeile = word;
+      } else if (zeile.isEmpty()) {
+        zeile = word;
+      } else {
+        zeile = zeile + " " + word;
+      }
+    }
+    finalText = finalText + zeile;
+    System.out.println(finalText + "\n");
+  }
 
-		while (toki.hasMoreTokens()) {
-			String word = toki.nextToken();
-			if (empty.length() >= (maxLineLength - word.length())) {
-				finalText = finalText + empty + "\n";
-				empty = word;
-			} else if (empty.isEmpty()) {
-				empty = word;
-			} else {
-				empty = empty + " " + word;
-			}
-		}
-		finalText = finalText + empty;
-		System.out.println(finalText);
-	}
-
-	public static void printRight(String aText, int maxLineLength) {
-		StringTokenizer toki = new StringTokenizer(text);
-		String empty = "";
+  public static void printRight(String text, int maxLineLength) {
+    StringTokenizer toki = new StringTokenizer(text);
     String zeile = "";
     int spaces = 0;
 
-		while (toki.hasMoreTokens()) {
-			String word = toki.nextToken();
-			// Zeile mit Länge 30
-			if (empty.length() >= (maxLineLength - word.length())) {
-			  zeile = empty;
-				// Anzahl der Leerzeichen
-				spaces = (maxLineLength - empty.length());
-				while (spaces != 0) {
-				  zeile = " " + zeile;
-					spaces--;
-				}
-			 	//finalText2 = zeile;
-        System.out.println(zeile);
-			  zeile = "";
-				empty = word; 
-        continue;
-			}
-			if (empty.isEmpty()) {
-				empty = word;
-			} else {
-				empty = empty + " " + word;
-			}
-		}
-	}
+    while (toki.hasMoreTokens()) {
+      String word = toki.nextToken();
+      if (zeile.length() >= (maxLineLength - word.length())) {
+        spaces = (maxLineLength - zeile.length());
+        while (spaces != 0) {
+          zeile = " " + zeile;
+          spaces--;
+        }
+        finalText2 = finalText2 + zeile + "\n";
+        zeile = word;
+      } else if (zeile.isEmpty()) {
+        zeile = word;
+      } else {
+        zeile = zeile + " " + word;
+      }
+    }
+    spaces = (maxLineLength - zeile.length());
+    while (spaces != 0) {
+      zeile = " " + zeile;
+      spaces--;
+    }
+    finalText2 = finalText2 + zeile;
+    System.out.println(finalText2 + "\n");
+  }
+
+  public static void printCentral(String text, int maxLineLength) {
+  StringTokenizer toki = new StringTokenizer(text);
+    String zeile = "";
+    int spaces = 0;
+
+    while (toki.hasMoreTokens()) {
+      String word = toki.nextToken();
+      if (zeile.length() >= (maxLineLength - word.length())) {
+        spaces = (maxLineLength - zeile.length()) / 2;
+        while (spaces != 0) {
+          zeile = " " + zeile + " ";
+          spaces--;
+        }
+        finalText3 = finalText3 + zeile + "\n";
+        zeile = word;
+      } else if (zeile.isEmpty()) {
+        zeile = word;
+      } else {
+        zeile = zeile + " " + word;
+      }
+    }
+    spaces = (maxLineLength - zeile.length()) / 2;
+    while (spaces != 0) {
+      zeile = " " + zeile + " ";
+      spaces--;
+    }
+    finalText3 = finalText3 + zeile;
+    System.out.println(finalText3 + "\n");
+  }
 }
